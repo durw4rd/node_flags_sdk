78bb7ee720a87e7a50209af4964e2050c7ffb9d9
Michal Fasanek <misa.fasa@gmail.com>
2021-04-07 18:19:03 +0200

    mvp

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# node_flags_sdk
+# node_v4.5.1_sdk
+
+**To get up and running:**
+1. Clone the repo: `git clone https://github.com/durw4rd/node_flags_sdk.git`
+2. Enter the new folder: `cd node_flags_sdk`
+3. Install dependencies: `npm install`
+4. Execute the app code: `node app.js`
+
+**Usage notes**
+1. **Line 6:** Define the log level ('debug' by default).
+2. **Line 9:** Provide the SDK key you want to use with the app.
+4. **Line 33:** Provide any attributes you might need for passing audience targeting of your flag rules.
--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 const optimizely = require('@optimizely/optimizely-sdk');
+const {v4: uuidv4 } = require('uuid');      // import a plugin for uuid creation
 
-optimizely.setLogger(optimizelySDK.logging.createLogger());
+// set logger
+optimizely.setLogger(optimizely.logging.createLogger());
 optimizely.setLogLevel('debug'); 
 
+// initialize the client
+const sdkKey = '85yWu7wPbz4d6secu1pEm';
+const userId = uuidv4();    // create a single uuid OR
+console.log(`[### CUSTOM LOG] User ID: ${userId}`);
+
 const optimizelyClientInstance = optimizely.createInstance({
-    sdkKey: '85yWu7wPbz4d6secu1pEm',
+    sdkKey,
     eventBatchSize: 10,
     eventFlushInterval: 2000,
+    datafileOptions: {
+        autoUpdate: true,
+        updateInterval: 10000,  // 10 seconds in milliseconds
+        urlTemplate: 'https://cdn.optimizely.com/datafiles/%s.json'
+    }
 });
 
+// use the SDK client
 optimizelyClientInstance.onReady({ timeout: 5000 }).then((result) => {
-    console.log(`[CUSTOM LOG] Optimizely successfully loaded: ${result.success}`);
+    // check if we're good to go
+    console.log(`[### CUSTOM LOG] Optimizely successfully loaded: ${result.success}`);
     if(!result.success) {
         console.log(result.reason);
     }
-});
+
+    // --------------- CORE ----------------------
+    let attributes = {
+        loggedIn: false,
+        burpees: 50
+    };
+
+    let user = optimizelyClientInstance.createUserContext(userId, attributes);
+
+    const flagDecision = user.decide('my_first_veloflag');
+    console.log(flagDecision);
 
 
-optimizelyClientInstance.close().then((result) => {
-    if (result.success === false) {
-        console.log(`[CUSTOM LOG] Failed to close the client instance. Reason: ${result.reason}`);
-    } else {
-        console.log(`[CUSTOM LOG] Safe to close the app: ${result.success}. Closing the app!`);
-        process.exit()      // exit the app
-    }
+    // --------------- CORE END ------------------
+
+    // closing the app
+    let timeout = 3000;
+    let timeoutInS = timeout/1000;
+    console.log(`[### CUSTOM LOG] Will close the app in ${timeoutInS} seconds`);
+    setTimeout(function() {
+        optimizelyClientInstance.close().then((result) => {
+            if (result.success === false) {
+                console.log(`[### CUSTOM LOG] Failed to close the client instance. Reason: ${result.reason}`);
+            } else {
+                console.log(`[### CUSTOM LOG] Safe to close the app: ${result.success}. Closing the app!`);
+                process.exit()      // exit the app
+            }
+        });
+    }, timeout);
 });
